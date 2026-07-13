a8d7479503e617e0860dd87ca7822e17d46f39a2
RapipAja <rafifprasetyo007@gmail.com>
2026-07-13 09:35:50 +0700

    update messages

--- src/data/messages.ts
+++ src/data/messages.ts
@@ -14,7 +14,12 @@ const messages: Message[] = [
         name: "Jizz",
         avatar: "/photos/Jizz-PP.png",
         message:
-            "Happy birthday, budee Rubby Venita! Semoga di usia yang baru menginjak 24 tahun, semoga budee selalu diberikan kesehatan, kebahagiaan yang tak putus, dan segala urusan serta impian budee dilancarkan oleh-Nya. Terima kasih selalu menjadi budee yang hebat dan inspiratif. Wish you all the very best 🩷🩷!!!",
+            `Happy birthday, budee Rubby Venita! 
+            
+            Semoga di usia yang baru menginjak 24 tahun, semoga budee selalu diberikan kesehatan, kebahagiaan yang tak putus, dan segala urusan serta impian budee dilancarkan oleh-Nya. 
+            Terima kasih selalu menjadi budee yang hebat dan inspiratif. 
+            
+            Wish you all the very best 🩷🩷!!!`,
     },
 
     {
@@ -30,7 +35,14 @@ const messages: Message[] = [
         name: "Zacky",
         avatar: "/photos/Zacky-PP.png",
         message:
-            "HAPPY LEVEL UP 24 BUDEE🥳🥳🥳🥳🥳, i hope your special day is a incredible as you are, ahahah pasti berat yaa bude selama ini sehingga sampe di titik 24, aku gatau hal baik dan hal buruk apa yang bude semasa ini lewati, but i always hope your days will always be good, dan semoga di umur yang ke 24 ini, mungkin banyak wishlist yang belum bude wujudkan, semoga terwujudkan. dan semoga dengan bertambahnya usia bude, akan bertambah juga rezeki, orang2 baik di sekitar bude dan semakin banyak di penuhi dengan banyak hal-hal baik, oh iyaa bude di umur yang ke 24 ini jangan berhenti untuk menjadi orang baik yaa, bangga deh bisa kenal dengan sosok orang yang baik, dan juga ngemong aku, jizz, al, mey, cila, bang rapip, kai ahahah. even kita online, aku tetep berharap di perjalanan bude berikutnya semoga di lancarkan terus urusannya, semoga bisa menjadi lebih2lah dari sebelumnya (apapun itu sih,yaa walaupun bude udah perfect ahahah), i hope my wish comes true to you,. and i think this is enough, ahahahaha HAPPY BIRTHDAY BUDEEE🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳",
+            `HAPPY LEVEL UP 24 BUDEE🥳🥳🥳🥳🥳, 
+            
+            I hope your special day is a incredible as you are, ahahah pasti berat yaa bude selama ini sehingga sampe di titik 24, aku gatau hal baik dan hal buruk apa yang bude semasa ini lewati, but i always hope your days will always be good, dan semoga di umur yang ke 24 ini, mungkin banyak wishlist yang belum bude wujudkan, semoga terwujudkan. 
+            
+            Dan semoga dengan bertambahnya usia bude, akan bertambah juga rezeki, orang2 baik di sekitar bude dan semakin banyak di penuhi dengan banyak hal-hal baik, oh iyaa bude di umur yang ke 24 ini jangan berhenti untuk menjadi orang baik yaa, bangga deh bisa kenal dengan sosok orang yang baik, dan juga ngemong aku, jizz, al, mey, cila, bang rapip, kai ahahah. even kita online, aku tetep berharap di perjalanan bude berikutnya semoga di lancarkan terus urusannya, semoga bisa menjadi lebih2lah dari sebelumnya (apapun itu sih,yaa walaupun bude udah perfect ahahah), 
+            
+            I hope my wish comes true to you and i think this is enough, 
+            ahahahaha HAPPY BIRTHDAY BUDEEE🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳`,
     },
 
     {
@@ -46,7 +58,14 @@ const messages: Message[] = [
         name: "Kai",
         avatar: "/photos/Kai-PP.png",
         message:
-            "happyy birthdayy budee rubyy, selamattt ulanggg tahunnn yangg ke 24 yaa budee,semoga di usia yang baru ini, semogaa budee selalu diberikann kebahagiaannn, kesehatannn, dan umurr yangg panjanggg.semogaaa Allah senantiasaaa memberikannn kekuatann dalanm menghadapiii setiappp tantangannn, melimpahkannn rezeki yang halal dan berlimpahhh, serta menghadirkan oranggg-oranggg baikk yangg hadirr di dalamm hidupp budee. tetaplah menjadi pribadi yang baik, rendah hati, dan penuh semangat. semoga hari ini menjadi awal dari tahun yang lebih indah, lebih bermakna, dan penuh dengan momen-momen yang membahagiakannn. selamattt bertambahh usiaa.semoga panjanggg umurrr, sehatt selaluu, suksess dalammm segalaaa urusann, dan selaluuu berada dalam lindungannn Allah. walaupunn budee nyebelinn tapii akuu tetepp sayangg samaa budee loveee uuu budeee ",
+            `happyy birthdayy budee rubyy, 
+            
+            selamattt ulanggg tahunnn yangg ke 24 yaa budee,semoga di usia yang baru ini, semogaa budee selalu diberikann kebahagiaannn, kesehatannn, dan umurr yangg panjanggg. 
+            
+            Semogaaa Allah senantiasaaa memberikannn kekuatann dalanm menghadapiii setiappp tantangannn, melimpahkannn rezeki yang halal dan berlimpahhh, serta menghadirkan oranggg-oranggg baikk yangg hadirr di dalamm hidupp budee. Tetaplah menjadi pribadi yang baik, rendah hati, dan penuh semangat. 
+            Semoga hari ini menjadi awal dari tahun yang lebih indah, lebih bermakna, dan penuh dengan momen-momen yang membahagiakannn. selamattt bertambahh usiaa.semoga panjanggg umurrr, sehatt selaluu, suksess dalammm segalaaa urusann, dan selaluuu berada dalam lindungannn Allah. 
+            
+            Walaupunn budee nyebelinn tapii akuu tetepp sayangg samaa budee loveee uuu budeee `,
     },
 
     {
@@ -54,7 +73,19 @@ const messages: Message[] = [
         name: "Mey",
         avatar: "/photos/Mey-PP.png",
         message:
-            "Happy Birthday! Semoga semua yang kamu cita-citakan segera terwujud. Tetap semangat ya! ✨",
+            `Halooo, Ka Rubby Happy birthday yaaa!!❤️
+
+Selamat ulang tahun yang ke 24 kakaa. Semoga di umur yang baru ini Ka Rubby selalu sehat, panjang umur, rezekinya makin lancar, semua urusannya dipermudah, dan apa pun yang lagi diusahain semoga satu per satu bisa tercapai.
+
+Semoga tahun ini lebih banyak senyumnya daripada capeknya, lebih banyak bahagianya daripada sedihnya, dan semoga selalu dikelilingi orang-orang yang sayang sama Ka Rubby, kayak akuuu🩷
+
+Makasiii juga yaa udah jadi kakak dari kitaa, kakak yang baik dan asik. Semogaa ngga bosen ngadepin adek adek Discord yang kadang suka random dan ngilang yaa🥹 
+
+Terakhir niii, doa yang paling pentingg. Semoga Ka Rubby cepet dipertemuin sama jodoh yang sesuai wishlist. Yang ganteng, berotot, mapan, green flag, penyayang, setia, rajin, humoris, pokoknya paket lengkap deh😋 😋  yang penting bikin Ka Rubby bahagia teruss. Aamiin🤍 
+
+Udah deh, segitu aja dari aku ya kaa. Sekali lagii, happy birthday yaa. Semoga umur 24 ini jadi tahun yang penuh berkah, penuh kejutan yang baik, dan semoga semua doa yang diucapin hari ini bener-bener terkabul. 
+
+Have a great day, Kakak cantikkuu🩷🩷🤍🤍`,
     },
 
     {
@@ -62,7 +93,14 @@ const messages: Message[] = [
         name: "Rapip",
         avatar: "/photos/Rapip-PP.png",
         message:
-            "Happy Birthday! Semoga semua yang kamu cita-citakan segera terwujud. Tetap semangat ya! ✨",
+            `Aku rasa semua doa dan harapanku untuk bude sama kayak anak - anak yang lain. 
+
+            Semoga kamu selalu bahagia, sehat, dan sukses dalam segala hal yang kamu lakukan. 
+            Semoga semua impian dan keinginan kamu tercapai, dan semoga kamu selalu dikelilingi oleh orang-orang yang mencintai dan mendukung kamu. 
+            
+            Happy birthday, Rubby Venita 
+
+            Semoga hari ini menjadi awal dari tahun yang luar biasa untuk kamu dimasa depan! ❤️ `,
     },
 
 ];
